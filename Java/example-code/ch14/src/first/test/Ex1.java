03a72ec2ab088505e85ae0e1362630f634ae5209
YuChia Ma <79797958+Mirai1129@users.noreply.github.com>
2024-04-14 23:26:35 +0800

    add example codes

---
+++ Java/example-code/ch14/src/first/test/Ex1.java
@@ -0,0 +1,12 @@
+package first.test;
+
+public class Ex1 {
+	public Ex1() {
+		System.out.println("執行類別Ex1的建構子");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Ex1.java儲存在ch14\\src\\first\\test資料夾，且");
+		System.out.println("Ex1.class儲存在ch14\\bin\\first\\test資料夾.");
+	}
+}
